fea4e8f5edbdb61d2ff80b210025ff03f2974af3
Philitachi <philitachi@example.com>
2026-03-12 14:36:34 +0800

    ForgotPassword Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import TermsAndConditions from './pages/termandconditons';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/terms" element={<TermsAndConditions />} />
         <Route path="/about" element={<HomePage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       </Routes>
     </Router>
   );
